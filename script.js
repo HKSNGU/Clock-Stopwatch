c77cf91f9df95388541b341b7c915a3e742ca2ba
Himanshu Kumar <93818810+HKSNGU@users.noreply.github.com>
2023-12-28 13:01:59 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//clock
+
 let hr = document.getElementById('hour');
 let min = document.getElementById('min');
 let sec = document.getElementById('sec');
@@ -20,3 +22,62 @@ function displayTime(){
 }
 
 setInterval(displayTime, 1000);
+
+//stopwatch
+
+let timerDisplay = document.querySelector('.timerDisplay');
+let stopBtn = document.getElementById('stopBtn');
+let startBtn = document.getElementById('startBtn');
+let resetBtn = document.getElementById('resetBtn');
+
+let msec = 0o0;
+let secs = 0o0;
+let mins = 0o0;
+
+let timerId = null;
+
+startBtn.addEventListener('click', function(){
+    if(timerId !== null){
+        clearInterval(timerId);
+    }
+    timerId = setInterval(startTimer, 10);
+    
+    startBtn.classList.add("hide");
+    stopBtn.classList.remove("hide");
+});
+
+stopBtn.addEventListener('click', function(){
+    clearInterval(timerId);
+
+    stopBtn.classList.add("hide");
+    startBtn.classList.remove("hide");
+});
+
+resetBtn.addEventListener('click', function(){
+    clearInterval(timerId);
+    timerDisplay.innerHTML = `00:00:00`;
+    msec = secs = mins = 0o0;
+
+    if(stopBtn.classList != "hide") stopBtn.classList.add("hide");
+    if(startBtn.classList == "hide") startBtn.classList.remove("hide");
+});
+
+function startTimer(){
+    msec++;
+    if(msec == 100){
+        msec = 0;
+        secs++;
+        if(secs == 60){
+            secs = 0;
+            mins++;
+        }
+    }
+
+    let msecString = msec < 10 ? `0${msec}` : msec;
+    let secsString = secs < 10 ? `0${secs}` : secs;
+    let minsString = mins < 10 ? `0${mins}` : mins;
+    
+
+    timerDisplay.innerHTML = `${minsString}:${secsString}:${msecString}`;
+
+}
